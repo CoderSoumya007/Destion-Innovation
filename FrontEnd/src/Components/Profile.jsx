17fe11531b38def2b94fd8749e28ada3068e03b0
SOUMYA RANJAN PANDA <116835983+CoderSoumya007@users.noreply.github.com>
2024-10-16 00:51:17 +0530

    profile update

--- FrontEnd/src/Components/Profile.jsx
+++ FrontEnd/src/Components/Profile.jsx
@@ -1,10 +1,12 @@
+import { User} from 'lucide-react';
+
 const ProfileContent = ({username,email}) => (
     <div className="p-6">
       <h2 className="text-2xl font-semibold mb-4">User Profile</h2>
       <div className="bg-white shadow-md rounded-lg p-6">
         <div className="flex items-center mb-4">
           <div className="w-16 h-16 rounded-full bg-gray-300 flex items-center justify-center">
-            <span className="text-2xl font-medium text-gray-600">JD</span>
+            <span className="text-2xl font-medium text-gray-600"><User className="text-white h-5 w-5" /></span>
           </div>
           <div className="ml-4">
             <h3 className="text-xl font-semibold">{username}</h3>
